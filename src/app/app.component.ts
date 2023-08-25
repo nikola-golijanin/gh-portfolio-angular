1b5cd1ac0a720c7176a29dd9ebe34d3e690238e9
Nikola Golijanin <nikola.golijanin@dinitcs.com>
2023-08-25 11:20:10 +0200

    Displaying personal info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GithubService } from './github.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'gh-portfolio';
-  username = 'nikola-golijanin';
+  username = '';
+
+  constructor(private githubService: GithubService) {}
+
+  ngOnInit(): void {
+    this.username = this.githubService.username;
+  }
+  
 
 }
---
+++ src/app/github.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from './user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GithubService {
+  readonly username = 'nikola-golijanin';
+  private apiUrl = 'https://api.github.com';
+
+  constructor(private http: HttpClient) {}
+
+  getUser(): Observable<User> {
+    return this.http.get<User>(`${this.apiUrl}/users/${this.username}`);
+  }
+}
