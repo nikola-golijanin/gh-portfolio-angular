d9155b48a0a5acec1f97601a983398b3d123cd16
Nikola Golijanin <nikola.golijanin@dinitcs.com>
2023-08-25 13:03:19 +0200

    Prerendering content during build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,17 @@ import { PersonalInfoComponent } from './personal-info/personal-info.component';
 import { PanelComponent } from './panel/panel.component';
 import { RepositoriesComponent } from './repositories/repositories.component';
 import { OrganizationsComponent } from './organizations/organizations.component';
+import { TransferHttpCacheModule } from '@nguniversal/common';
 
 @NgModule({
-  declarations: [AppComponent, PersonalInfoComponent, PanelComponent, RepositoriesComponent, OrganizationsComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    PersonalInfoComponent,
+    PanelComponent,
+    RepositoriesComponent,
+    OrganizationsComponent,
+  ],
+  imports: [BrowserModule, HttpClientModule, TransferHttpCacheModule],
   providers: [],
   bootstrap: [AppComponent],
 })
