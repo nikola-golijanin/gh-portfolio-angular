e6e3b17a23ce1593d483bd82ae53626a1ae6d7e9
Nikola Golijanin <nikola.golijanin@dinitcs.com>
2023-08-25 11:38:51 +0200

    Listing user repositories

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -2,17 +2,22 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from './user';
+import { Repository } from './repository';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GithubService {
   readonly username = 'nikola-golijanin';
-  private apiUrl = 'https://api.github.com';
+  private userUrl = 'https://api.github.com/users/' + this.username;
 
   constructor(private http: HttpClient) {}
 
   getUser(): Observable<User> {
-    return this.http.get<User>(`${this.apiUrl}/users/${this.username}`);
+    return this.http.get<User>(this.userUrl);
+  }
+
+  getRepos(): Observable<Repository[]> {
+    return this.http.get<Repository[]>(this.userUrl + '/repos');
   }
 }
